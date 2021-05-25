2d3f93e2878adb861a94d65d19ff1a1b61d83b2b
Roshaan Singh <swaggsikh@gmail.com>
2021-05-25 01:50:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 def prime?(integer)
   n = integer.to_i
-  if integer.to_i < 0 
+  if n < 0 
     return false
   elsif n == 0 
     return false
-  elsif n == 1 
-  return false
-  elsif (n / n == 1) && (n / 1 == n)
+  elsif (n / n == 1) && (n / 1 == n) 
     return true
   else
     return false 
