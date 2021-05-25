a9e264a2e85c7142b1b7e205a66bfd85543c1e91
Roshaan Singh <swaggsikh@gmail.com>
2021-05-25 01:25:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   n = integer.to_i
   if integer.to_i < 0 
     return false
-  elsif (n / n == 1) && (n / 1 == n) && (n !== 0)
+  elsif (n / n == 1) && (n / 1 == n) && (n != 0)
     return true
   else
     return false 
